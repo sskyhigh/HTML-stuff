01ecbf1eed009055b65fbd38627c6536d6541e26
sskyhigh <windowshao_123@yahoo.com>
2023-01-14 21:47:47 -0500

    update
    Tutorials from Kevin Powell

---
+++ js/cone.js
@@ -0,0 +1,7 @@
+gsap.to('div', {
+    // background: `conic-gradient(from 1.25turn at 50% 50%, black, 10deg, black, 350deg, green)`,
+    "--turn": "1.25turn",
+    repeat: -1,
+    duration: 10,
+    ease: 'none'
+  })
